cbccda9a06c68bc39a951558b48cceb9ad342601
SaintBrady <brayden.dyke@gmail.com>
2019-04-11 16:11:01 -0500

    File Transfer

---
+++ Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    Vector2 deltaScale;
+    private float deltaX, deltaY;
+    public float sensitivity = 2.0f;
+    public float smoothing = 1.5f;
+
+    GameObject character;
+
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+        deltaScale = new Vector2(sensitivity * smoothing, sensitivity * smoothing);
+        deltaX = 0f;
+        deltaY = 0f;
+    }
+
+    void LateUpdate()
+    {
+        deltaX += Input.GetAxis("Mouse X") * sensitivity * smoothing;
+        deltaY += Input.GetAxis("Mouse Y") * sensitivity * smoothing;
+        deltaY = Mathf.Clamp(deltaY, -90f, 90f);
+        smoothV.x = Mathf.Lerp(smoothV.x, deltaX, 1.0f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, deltaY, 1.0f / smoothing);
+
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.Euler(-deltaY, 0f, 0f);
+        character.transform.localRotation = Quaternion.Euler(0f, deltaX, 0f);
+    }
+}
---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float jumpForce = 2.0f;
+    private bool isGrounded = true;
+    private Rigidbody rb;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Movement Input Start
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0.0f, translation);
+        // Movement Input End
+
+        // Jumping Input Start
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0f, jumpForce, 0f));
+            isGrounded = false;
+        }
+        // Jumping Input End
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+}
